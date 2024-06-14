7a7f1a3535a8338930af32942c1d1bfd80a553c2
mulchus <mulch@list.ru>
2024-06-14 15:22:54 +0300

    finish start app

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.name
---
+++ myproject/myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import ItemListCreate, ItemDetail
+
+
+urlpatterns = [
+    path('items/', ItemListCreate.as_view(), name='item-list-create'),
+    path('items/<int:pk>/', ItemDetail.as_view(), name='item-detail'),
+]
